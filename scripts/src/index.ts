afedb4bfd1e509817a335c1798453af22b0bdb9d
Andcool-Systems <drolll2007@gmail.com>
2025-08-01 21:24:07 +0300

    test: Test #5

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -1,8 +1,11 @@
 import { getListDirs } from './utils/fs-util';
 
 const main = async () => {
+    console.log(process.env);
     console.log(process.cwd());
     console.log(await getListDirs('.'));
+
+    // Допустим я что-то написал тут очень важное что пиздец умереть смерть
 };
 
 void main();
